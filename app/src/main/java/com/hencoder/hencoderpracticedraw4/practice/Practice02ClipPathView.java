91bd3b1be6bab6a8549b4e97ee1c61184bdea9b8
glk <77833601@qq.com>
2018-07-24 10:51:22 +0800

    练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice02ClipPathView.java
@@ -7,6 +7,8 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -39,7 +41,34 @@ public class Practice02ClipPathView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.save();
+        Path path = new Path();
+        path.addCircle(400, 400, 150, Path.Direction.CW);
+        canvas.clipPath(path);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+
+//        //Xfermode 方法
+//        int sc = canvas.saveLayer(null, null, Canvas.ALL_SAVE_FLAG);
+//        canvas.save();
+//        Path path1 = new Path();
+//        path1.addCircle(800, 400, 150, Path.Direction.CW);
+//        canvas.clipPath(path1);
+//        canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+//        canvas.restore();
+//        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+//        canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+//        paint.setXfermode(null);
+//        canvas.restoreToCount(sc);
+
+        canvas.save();
+        Path path1 = new Path();
+        path1.setFillType(Path.FillType.INVERSE_WINDING);
+        path1.addCircle(800, 400, 150, Path.Direction.CW);
+
+        canvas.clipPath(path1);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
